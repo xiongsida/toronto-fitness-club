e82d24346ff78b5fbad109cee4f9d8f6799af85c
Sida Xiong <sida.xiong1211@gmail.com>
2022-12-05 14:56:08 -0500

    modify django pagination class to pass more page info to frontend

--- PF/PB/PB/studios/pagination.py
+++ PF/PB/PB/studios/pagination.py
@@ -1,9 +1,31 @@
 from rest_framework import pagination
+from rest_framework.response import Response
 
 class CustomPagination(pagination.PageNumberPagination):
-    page_size = 2
+    page_size = 5
     page_size_query_param = 'page_size'
     max_page_size = 30
+    
+    def get_from(self):
+        return int((self.page.paginator.per_page * self.page.number) - self.page.paginator.per_page + 1)
+
+    def get_to(self):
+        return self.get_from() + int(len(self.page.object_list)) - 1
+    
+    def get_paginated_response(self, data):
+        return Response({
+            'page':{
+                'totalItems': self.page.paginator.count,
+                'totalPages': self.page.paginator.num_pages,
+                'pageSize': self.page_size,
+                'currentPage': self.page.number,
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link(),
+                'from': self.get_from(),
+                'to': self.get_to(),
+            },
+            'results': data,
+        })
 
 
 class SubscriptionPagination(pagination.PageNumberPagination):
