536d9efbf2d7f0e28b68cc889b126c754ba823f4
Sida Xiong <sida.xiong1211@gmail.com>
2022-12-07 17:06:32 -0500

    modify backend to provide user's class schedule and history to make frontend action button not shown in some situations

--- PF/PB/PB/classes/urls.py
+++ PF/PB/PB/classes/urls.py
@@ -1,10 +1,12 @@
 app_name = 'classes'
 from django.urls import path
-from classes.views import ClassesListView, ClassEnrollView, ClassDropView, ClassParentView
+from classes.views import ClassesListView, ClassEnrollView, ClassDropView, ClassParentView,ClassScheduleView
 
 urlpatterns = [
     path('classes', ClassesListView.as_view(), name='classes-list'),
     path('classes/<int:class_id>/enroll', ClassEnrollView.as_view(), name='class-enroll'),
     path('classes/<int:class_id>/drop', ClassDropView.as_view(), name='class-drop'),
-    path('allclassparents', ClassParentView.as_view(), name='class-all-parents'),
+    path('allclassparents', ClassParentView.as_view(), name='class-all-parents-for-frontend'),
+    path('alluserschedules',ClassScheduleView.as_view(),name='alluserschedules-for-frontend'),
+    path('alluserhistoryclasses',ClassScheduleView.as_view(),name='alluserhistoryclasses-for-frontend')
 ]
