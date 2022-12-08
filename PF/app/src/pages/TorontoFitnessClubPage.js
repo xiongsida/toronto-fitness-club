250a9addffaa3d70560c2435f5e32109ab3c40a8
JhongLuo <1999luozhonghao@gmail.com>
2022-12-08 15:46:45 -0500

    src/pages/TorontoFitnessClubPage.js

--- PF/app/src/pages/TorontoFitnessClubPage.js
+++ PF/app/src/pages/TorontoFitnessClubPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import tw from "twin.macro";
 import Hero from "./TFCHero.js";
 import Testimonial from "../components/testimonials/TFCTestimonials.js";
-
+import TFCPrice from "./TFCPrice.js";
 
 export default () => {
     const HighlightedText = tw.span`bg-primary-500 text-gray-100 px-4 transform inline-block`;
@@ -10,6 +10,7 @@ export default () => {
         <>
             <Hero />
             <Testimonial />
+            <TFCPrice />
             <div className="wordsundercard">
                 Toronto Fitness Club
             </div>
